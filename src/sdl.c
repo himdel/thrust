995a0e70fa8117cffa1df69cabf52e67dcbbe895
Martin Hradil <himdel@seznam.cz>
2011-11-04 19:27:14 +0000

    new sdl stubs
    ack-grep -hB1 '^\w[\w ]+\(.*\)\s*$' svga.c  X11.c | sed -e 's/^\d\+://' -e 's/^.*://' | grep -v ^-- | perl -ne 'if ($b) { print $b . $_; $b = ""; } else { chomp; $b = "$_ "; }' | sort -u | perl -ne 's/$/ { }/; for (split(/ /, $_)) { print "$_\n"; }' > sdl.c

---
+++ src/sdl.c
@@ -0,0 +1,96 @@
+// sdl output
+
+Pixmap
+create_icon(void)
+{
+}
+
+RETSIGTYPE
+signalerrorhandler(int num)
+{
+}
+
+char *
+graphicsname(void)
+{
+}
+
+int
+graphicsclose(void)
+{
+}
+
+int
+graphicsinit(int argc, char **argv)
+{
+}
+
+static int
+GetShmPixmap(VScreenType *pixmap)
+{
+}
+
+static int
+shmerrorhandler(Display *d, XErrorEvent *e)
+{
+}
+
+static void
+shmfree(void)
+{
+}
+
+void
+CopyToScreen(void)
+{
+}
+
+void
+SetCustomPalette(void)
+{
+}
+
+void
+clearscr(void)
+{
+}
+
+void
+delaygame(long usec)
+{
+}
+
+void
+displayscreen(void)
+{
+}
+
+void
+fade_in(void)
+{
+}
+
+void
+fade_out(void)
+{
+}
+
+void
+fadepalette(int first, int last, byte *RGBtable, int fade, int flag)
+{
+}
+
+void
+graphics_preinit(void)
+{
+}
+
+void
+putpixel(int x, int y, byte color)
+{
+}
+
+void
+syncscreen(void)
+{
+}
